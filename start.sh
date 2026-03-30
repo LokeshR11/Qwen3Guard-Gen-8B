832391de59a3e239b808e5c13b1c8ba8c329b80f
LokeshR11 <98221356+LokeshR11@users.noreply.github.com>
2026-03-30 18:01:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,23 +3,14 @@ set -e
 
 echo "Starting vLLM OpenAI server..."
 
-
+export VLLM_USE_V1=0
+export VLLM_WORKER_MULTIPROC_METHOD=spawn
 export HOME=/tmp
 export HF_HOME=/tmp/huggingface
-export TRANSFORMERS_CACHE=/tmp/huggingface
 export VLLM_CACHE_ROOT=/tmp/vllm
 
+mkdir -p /tmp/huggingface /tmp/vllm
 
-mkdir -p /tmp/huggingface
-mkdir -p /tmp/vllm
-
-# Debug
-echo "HF_HOME=$HF_HOME"
-echo "TRANSFORMERS_CACHE=$TRANSFORMERS_CACHE"
-echo "VLLM_CACHE_ROOT=$VLLM_CACHE_ROOT"
-echo "HOME=$HOME"
-
-# GPU diagnostics
 nvidia-smi || echo "No GPU detected"
 
 python3 - <<EOF
@@ -30,15 +21,14 @@ if torch.cuda.is_available():
     print("VRAM:", torch.cuda.get_device_properties(0).total_memory // 1024**3, "GB")
 EOF
 
-# 🚀 Start server
 exec python3 -m vllm.entrypoints.openai.api_server \
-    --model /models/Qwen3Guard-Gen-8B \
-    --host 0.0.0.0 \
-    --port 8080 \
-    --dtype float16 \
-    --max-model-len 256 \
-    --gpu-memory-utilization 0.90 \
-    --tensor-parallel-size 1 \
-    --trust-remote-code \
-    --max-num-seqs 1  \
-    --enforce-eager
+  --model /models/Qwen3Guard-Gen-8B \
+  --host 0.0.0.0 \
+  --port 8080 \
+  --dtype float16 \
+  --max-model-len 256 \
+  --gpu-memory-utilization 0.80 \
+  --tensor-parallel-size 1 \
+  --trust-remote-code \
+  --max-num-seqs 1 \
+  --enforce-eager
